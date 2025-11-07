05df1f26756e92b2d5e04a39dcff679593a819ab
dee-are-here <david.ward.uno@gmail.com>
2025-11-07 11:13:18 -0600

    updated files

--- terraform-js/main.tf
+++ terraform-js/main.tf
@@ -23,7 +23,6 @@ resource "aws_s3_bucket_public_access_block" "nextjs_bucket_public_access_block"
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
-
 }
 
 # Bucket ACL
@@ -43,8 +42,8 @@ resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
 resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
   bucket = aws_s3_bucket.nextjs_bucket.id
 
-  policy = jsondecode(({
-    version = "2012-10-17"
+  policy = jsonencode(({
+    Version = "2012-10-17"
     Statement = [
       {
         Sid       = "PublicReadGetObject"
@@ -55,4 +54,66 @@ resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
       }
     ]
   }))
+}
+
+# Origin Access Identity CloudFront
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for Next.JS portfolio site"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "nextjs.distribution" {
+  origin {
+    domain_name = aws_s3_bucket_bucket.bucket_regional_doamin_name
+    origin_id   = "S3-nextjs-portfolio-bucket"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Next.js_portfolio_site"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-nextjs-portfolio-bucket"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name        = "Portfolio CloudFront"
+    Environment = "Production"
+  }
+
 }
